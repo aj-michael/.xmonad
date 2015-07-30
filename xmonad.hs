e8f67bc6902429312ea771841658d537f30448f5
Adam Michael <adam@ajmichael.net>
2015-07-30 15:47:32 -0700

    Basic upgrades.

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,15 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocs
+import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/adam/.xmobarrc"
     xmonad $ defaultConfig
-
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , modMask = mod4Mask
+        , borderWidth = 1
+        }
